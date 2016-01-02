156d2cf13d415f827837e966ef9ec2de22669651
Casey Haralson <casey.haralson@gmail.com>
2016-01-02 14:35:10 -0800

    working on a easy starting point for people who don't know how to get started with the regular library

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# Slipn
+# Slipn - Starter Pack
+
+This starter pack should help to get started quickly.
+
+
+## Getting Started
+
+**First**, [download the files.](https://github.com/CaseyHaralson/slipn/archive/0.1.0.zip)
+
+**Second**, open downloaded file and then the "myslideshow.html" file to see the starting point.
+
+There are three slides included.  One contains an image with some text and a header.
+One contains an image and some text.  One contains just an image.
+
+The slides also have some icons and text for the previous and next actions.
+There is also a fullscreen button that will display if the browser can support fullscreen webpages.
+
+**Third**, open the "myslideshow.html" file in a text editor. There are three important things to notice here.  
+
+The first is the templates section. You can use these as a starting point for your own slides.
+
+The second is the slides section. This is where you will actually create the slide data for your show.
+
+The third is the 
+
+
+
+
+
 
 Slipn is a slide show library for interactive presentations.
 The library can do several things:
---
+++ js/myslideshow.js
@@ -0,0 +1,54 @@
+﻿(function () {
+
+    /////////////////////////////////////
+    // FULLSCREEN
+    /////////////////////////////////////
+    function showFullScreenOption() {
+        var optionExists = false;
+        var element = document.body;
+        var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullscreen;
+        if (requestMethod) optionExists = true;
+
+        if (optionExists && !isFullscreen()) {
+            $('.fullscreen').fadeIn('slow');
+            $(".fullscreen").click(function () {
+                var elem = document.body; // Make the body go full screen.
+                requestFullScreen(elem);
+            });
+        }
+    };
+    function requestFullScreen(element) {
+        // Supports most browsers and their versions.
+        var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullscreen;
+        if (requestMethod) { // Native full screen.
+            requestMethod.call(element);
+            $('.fullscreen').fadeOut('slow');
+        }
+        /*else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }*/
+    };
+    function isFullscreen() {
+        if ((screen.availHeight || screen.height - 30) <= window.innerHeight) return true;
+        return false;
+    };
+    /////////////////////////////////////
+
+
+
+    function closeButtonEvents() {
+        $('.slide .container:hidden').fadeIn(1500);
+        $('.close').click(function () {
+            $(this).parent().fadeOut('fast');
+        });
+    };
+
+
+    $(window).load(function () {
+        $('#slipn').on('slideLoading', showFullScreenOption);
+        $('#slipn').on('slideLoading', closeButtonEvents);
+    });
+})();
